08a1d17cddab399b587873cd75b205916ebc2277
a4studios04 <studio.a4global@gmail.com>
2026-06-29 19:28:54 +0530

    push

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://xyro.art',
   output: 'static',
-  adapter: cloudflare(),
   integrations: [sitemap()]
 });
