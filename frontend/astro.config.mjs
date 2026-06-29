c167e06c2f2340602c17badde189e21ea8a224f1
a4studios04 <studio.a4global@gmail.com>
2026-06-29 20:57:35 +0530

    gu

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://xyro.art',
-  output: 'server',
-  adapter: cloudflare({
-    inspectorPort: false,
-    prerenderEnvironment: 'node'
-  }),
-  integrations: [sitemap()]
+  site: "https://xyro.art",
+  output: "server",
+  adapter: cloudflare(),
+  integrations: [sitemap()],
 });
